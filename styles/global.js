30318fedee20619426db9044ee3ec8c68ec538d4
Wamziss <hannahmwangi551@gmail.com>
2024-06-22 18:32:20 +0300

    Created the globalStyles File.

---
+++ styles/global.js
@@ -0,0 +1,52 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  Primary: {
+    backgroundColor: "#ffffff",
+  },
+  PrimaryText: {
+    color: "#333",
+  },
+  Secondary: {
+    backgroundColor: "#dff2ff",
+  },
+  SecondaryText: {
+    color: "#dff2ff",
+  },
+  Accent: {
+    backgroundColor: "#f9c70c",
+  },
+  AccentText: {
+    color: "#f9c70c",
+  },
+  Danger: {
+    backgroundColor: "#ff4c4c",
+  },
+  DangerText: {
+    color: "#ff4c4c",
+  },
+  Warning: {
+    backgroundColor: "#ffa500",
+  },
+  WarningText: {
+    color: "#ffa500",
+  },
+  Success: {
+    backgroundColor: "#4caf50",
+  },
+  SuccessText: {
+    color: "#4caf50",
+  },
+  Alert: {
+    backgroundColor: "#ff7043",
+  },
+  AlertText: {
+    color: "#ff7043",
+  },
+  Logo: {
+    backgroundColor: "#00707ac",
+  },
+  LogoText: {
+    color: "#00707ac",
+  },
+});
